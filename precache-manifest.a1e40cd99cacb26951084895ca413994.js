9bf443fb2d9fcdc06e7c17705ce22049d8817685
Dan6erbond <moravrav@gmail.com>
2019-11-26 00:09:03 +0100

    build

---
+++ precache-manifest.a1e40cd99cacb26951084895ca413994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c466d0e60bec49722ba90c463a83530a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea5e48699cfa920aba0",
+    "url": "/static/css/main.48fac1d3.chunk.css"
+  },
+  {
+    "revision": "ad11ea2abe9ad21847f5",
+    "url": "/static/js/2.552d241d.chunk.js"
+  },
+  {
+    "revision": "9ea5e48699cfa920aba0",
+    "url": "/static/js/main.42bc6bb0.chunk.js"
+  },
+  {
+    "revision": "c1b2844d4cd8099686a5",
+    "url": "/static/js/runtime-main.6478e668.js"
+  }
+]);
