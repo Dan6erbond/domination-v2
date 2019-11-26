4f56ee69574f02097d888f7a5eb4ec2f7a7e08c4
Dan6erbond <moravrav@gmail.com>
2019-11-26 15:53:22 +0100

    enhanced world map loading to include most countries

--- src/src/Domination.js
+++ src/src/Domination.js
@@ -3,6 +3,7 @@ import './Domination.css';
 import './BlankMap-World.css';
 
 var XMLParser = require('react-xml-parser');
+var parser = new XMLParser();
 
 class Country extends React.Component {
   /* constructor(props){
@@ -11,7 +12,7 @@ class Country extends React.Component {
 
   render() {
     return (
-        <path id="ss" className={this.props.red ? this.props.class + " red" : this.props.class} d={this.props.path}
+        <path id={this.props.id} className="landxx" d={this.props.path}
               onMouseEnter={this.props.onMouseEnter} onMouseLeave={this.props.onMouseLeave} >
           <title>{this.props.name}</title>
         </path>
@@ -22,49 +23,86 @@ class Country extends React.Component {
 export default class Domination extends React.Component {
   constructor(props){
     super(props);
-    this.state = {countries: [], currentCountry: 1}
+    this.state = {countries: [], currentCountry: null}
   }
 
   componentDidMount(){
     fetch('./BlankMap-World.svg')
-    .then((r) => r.text())
-    .then(text  => {
-      var parser = new XMLParser();
+    .then((r) => r.text()).then(text  => {
       var xml = parser.parseFromString(text);
-      var objects = xml.getElementsByTagName('path');
-      // objects.push(...xml.getElementsByTagName('g'));
-      var cs = [];
-      for (var i in objects){
-        var country = objects[i];
-        if ("class" in country.attributes && country.attributes.class.includes("landxx") && country.children.length >= 1) {
-          country.name = country.getElementsByTagName("title")[0].value;
-          cs.push(country);
+
+      var paths = xml.getElementsByTagName('path');
+      var gs = xml.getElementsByTagName('g');
+
+      var ids = [];
+      var countries = [];
+      var ocean;
+      for (let i in paths){
+        let path = paths[i];
+        if (!("class" in path.attributes)) {
+          continue;
+        } else if (path.attributes.class.includes("landxx") && path.children.length >= 1) {
+          path.name = path.getElementsByTagName("title")[0].value;
+          if (!ids.includes(path.attributes.id)) {
+            countries.push(path);
+            ids.push(path.attributes.id);
+          }
+        } else if (path.attributes.class.includes("oceanxx")) {
+          ocean = <path id="ocean" className="oceanxx" d={path.attributes.d} />;
         }
       }
-      this.setState({countries: cs});
+      for (let i in gs) {
+        var g = gs[i];
+        if (!("class" in g.attributes)) {
+          continue;
+        } else if (g.attributes.class.includes("landxx") && g.children.length >= 1 && g.getElementsByTagName("title").length >= 1) {
+          let paths = g.getElementsByTagName("path");
+          let found = false;
+          for (let j in paths) {
+            let path = paths[j];
+            if (!("class" in path.attributes)) {
+              continue;
+            } else if (path.attributes.class.includes("landxx")) {
+              found = true;
+              path.name = g.getElementsByTagName("title")[0].value;
+              path.attributes.id = g.attributes.id;
+              if (!ids.includes(path.attributes.id)) {
+                countries.push(path);
+                ids.push(path.attributes.id);
+              }
+            }
+          }
+          if (!found) {
+            let path = paths[0];
+            path.name = g.getElementsByTagName("title")[0].value;
+            path.attributes.id = g.attributes.id;
+            if (!ids.includes(path.attributes.id)) {
+              countries.push(path);
+              ids.push(path.attributes.id);
+            }
+          }
+        }
+      }
+      this.setState({countries: countries, ocean: ocean});
     });
   }
 
   render() {
-    /* var countries = [];
-    for (var i in this.state.countries) {
-      var country = this.state.countries[i];
-      countries.push(<Country name={country.name} key={country.attributes.id} class="landxx ss" path={country.attributes.d}
-              hoverFun={this.hoverCountry} red={i === this.state.currentCountry}
-              onMouseEnter={(i) => this.mouseEnter(i)} onMouseLeave={() => this.mouseLeave()} />);
-    } */
     return (
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-30 61 2754 1398" width="100%">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-30 61 2754 1398" className="map">
+          {"ocean" in this.state ? this.state.ocean : null}
           {this.state.countries.map((country, index) =>
-            <Country name={country.name} key={country.attributes.id} class="landxx ss" path={country.attributes.d}
-                    hoverFun={this.hoverCountry} red={index === this.state.currentCountry}
+            <Country name={country.name} key={country.attributes.id} id={country.attributes.id}
+                    path={country.attributes.d} hoverFun={this.hoverCountry}
                     onMouseEnter={() => this.mouseEnter(index)} onMouseLeave={() => this.mouseLeave()} />
           )}
         </svg>
 
         <div className="bottomBar">
-          <div>{this.state.countries.length > 0 && this.state.currentCountry != null ? this.state.countries[this.state.currentCountry].name : ""}</div>
+          <div>
+            {this.state.countries.length > 0 && this.state.currentCountry != null ? this.state.countries[this.state.currentCountry].name : ""}
+          </div>
         </div>
       </div>
     );
